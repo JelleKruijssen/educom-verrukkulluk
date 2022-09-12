2fd8e999bdc0f85d9bc95e88643edf8883ee2b6a
JelleKruijssen <jellepapenhoven@live.nl>
2022-09-12 13:56:39 +0200

    VER-vsc-keukentype-JK-V1.php (#7)

---
+++ lib/keukentype.php
@@ -0,0 +1,17 @@
+<?php
+// Zorgt voor de connectie tussen de database en de keuze voor artikel
+class keukentype {
+    private $connection;
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
+    public function selecteerKeukenType($keukentype_id) {
+        $sql = "select * from kitchentype where id = $keukentype_id";
+        $result = mysqli_query($this->connection, $sql);
+        $keukentype = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        return($keukentype);
+    }
+
+}
+
+?>
