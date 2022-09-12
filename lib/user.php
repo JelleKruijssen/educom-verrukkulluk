d6f928eb81c6b3061939962add08a1ae71bb6dc1
JelleKruijssen <jellepapenhoven@live.nl>
2022-09-12 13:47:27 +0200

    VER-vsc-User-JK-V1.php (#6)

---
+++ lib/user.php
@@ -0,0 +1,18 @@
+<?php
+// Zorgt voor de connectie tussen de database en de keuze voor user
+class user {
+    private $connection;
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
+
+    public function selecteerUser($user_id) {
+        $sql = " select * from user where id = $user_id";
+        $result = mysqli_query($this->connection, $sql);
+        $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        return($user);
+    }
+
+}
+
+?>
