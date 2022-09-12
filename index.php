95c219eb1c21e458f74fb50ff797579819018107
JelleKruijssen <jellepapenhoven@live.nl>
2022-09-12 13:41:47 +0200

    VER-vsc-User-JK-V1.php (#6)

--- index.php
+++ index.php
@@ -2,15 +2,46 @@
 
 require_once("lib/database.php");
 require_once("lib/artikel.php");
+require_once("lib/user.php");
 
 /// INIT
 $db = new database();
 $art = new artikel($db->getConnection());
+$usr = new user($db->getConnection());
 
 
 /// VERWERK 
 // Selecteer een artikel dat word weergegeven op de site
-$data = $art->selecteerArtikel(2);
+// selecteerArtikel moet nog aangepast worden zodat er met een variabele gezocht kan worden
+$artikel = $art->selecteerArtikel(2);
+
+// selecteerUser moet nog aangepast worden zodat het juiste account word gekozen na inloggen
+$user = $usr->selecteerUser(2);
+
+// select * from ingredient
+//  where gerecht_id = $id
+// $return = []
+// ingredient while ($row = mysqli_fetch_array()
+
+// while() {
+// 
+// $artikel_id = $row["artikel_id"]
+// $artikel = $this->art->selectArtikel($artikel_id);
+// $return[] = [
+// "id" => $row["id],
+// "gerecht_id" => $row["gerecht_id],
+// "naam" => $artikel["naam"]
+// ];  
+// }
+// 
+// priv db
+// priv art
+
+// return($return)
 
 /// RETURN
-var_dump($data);
+var_dump($artikel); 
+echo "<br>";
+var_dump($user);
+
+?>
