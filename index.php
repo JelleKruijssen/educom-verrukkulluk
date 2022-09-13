c16cac9da2a2bf43113706b6383480b8c7f831f4
JelleKruijssen <jellepapenhoven@live.nl>
2022-09-13 14:51:10 +0200

    receptinfo (#8)

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("lib/artikel.php");
 require_once("lib/user.php");
 require_once("lib/keukentype.php");
 require_once("lib/ingredient.php");
+require_once("lib/receptinfo.php");
 
 /// INIT
 $db = new database();
@@ -12,6 +13,7 @@ $art = new artikel($db->getConnection());
 $usr = new user($db->getConnection());
 $kt = new keukentype($db->getConnection());
 $ing = new ingredient($db->getConnection());
+$rpi = new receptinfo($db->getConnection());
 
 
 /// VERWERK 
@@ -23,6 +25,8 @@ $user = $usr->selecteerUser(2);
 $keukentype = $kt->selecteerKeukenType(1);
 // ingredienten
 $ingredient = $ing->selecteerIngredient(1);
+// receptinfo
+$receptinfo = $rpi->selecteerReceptinfo(2, "F");
 
 /// RETURN
 // var_dump($artikel); 
@@ -30,8 +34,11 @@ $ingredient = $ing->selecteerIngredient(1);
 // var_dump($user);
 // echo "<br>";
 // var_dump($keukentype);
+// echo "<br>";
+// echo "<pre>";
+// var_dump($ingredient);
 echo "<br>";
 echo "<pre>";
-var_dump($ingredient);
+var_dump($receptinfo);
 
 ?>
---
+++ lib/receptinfo.php
@@ -0,0 +1,88 @@
+<?php
+
+class receptinfo {
+
+
+    private $connection;
+    private $usr;
+    private $favoriet;
+
+    public function __construct($connection) {
+        $this->connection = $connection;
+        $this->usr = new User($connection);
+    }
+
+    private function selectUser($user_id){
+        $user = $this->usr->selecteerUser($user_id);
+
+        return ($user);
+    }
+
+
+    public function selecteerReceptinfo($recipe_id, $record_type) {
+        $sql = "select * from recipe_info where recipe_id = '$recipe_id' AND record_type = '$record_type'";
+        $result = mysqli_query($this->connection, $sql);
+        $arr = [];
+
+        if ($record_type == "O") {
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                $user_id = $row['user_id'];
+                $user = $this->selectUser($user_id);
+                $arr[] = [ 
+                    "id"=>$row['id'],
+                    "recipe_id"=>$row['recipe_id'],
+                    "record_type" =>$row['record_type'],
+                    "user_id" =>$row['user_id'],
+                    "date" =>$row['date'],
+                    "numeric_field" =>$row['numeric_field'],
+                    "text_field" =>$row['text_field'],
+                ];
+            
+            return $arr;
+            }
+        }elseif ($record_type == "F") {
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                $user_id = $row['user_id'];
+                $user = $this->selectUser($user_id);
+                $arr[] = [
+                    "id" => $row['id'],
+                    "recipe_id" =>$row['recipe_id'],
+                    "record_type" =>$row['record_type'],
+                    "user_id" =>$row['user_id'],
+                    "date" =>$row['date'],
+                ];
+            return $arr;
+            
+
+///////////////// Toevoegen en verwijderen van favorieten 
+            }
+        }elseif ($record_type == "W") {
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                $arr[] = [
+                    "id" => $row['id'],
+                    "recipe_id" => $row['recipe_id'],
+                    "record_type" =>$row['record_type'],
+                    "date" =>$row['date'],
+                    "numeric_field" =>$row['numeric_field'],
+                ];
+            }
+            return $arr;
+        }else {
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                $arr[] = [
+                    "id" => $row['id'],
+                    "recipe_id" => $row['recipe_id'],
+                    "record_type" =>$row['record_type'],
+                    "date" =>$row['date'],
+                    "numeric_field" =>$row['numeric_field'],
+                    "text_field" =>$row['text_field'],
+                ];
+            }
+        return $arr;
+        }
+
+    }
+}
+
+?>
+
